665ecaa798c4f563495ea4067bdd7789a740ad5f
Bet4 <0xbet4@gmail.com>
2026-05-04 09:52:43 +0800

    Add paseo-desktop package (#59)

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -16,6 +16,7 @@
   neko-master = pkgs.callPackage ./neko-master/package.nix { };
   netcatty = pkgs.callPackage ./netcatty/package.nix { };
   nexterm = pkgs.callPackage ./nexterm/package.nix { };
+  paseo-desktop = pkgs.callPackage ./paseo-desktop/package.nix { };
   qbit = pkgs.callPackage ./qbit/package.nix { };
   shell360 = pkgs.callPackage ./shell360/package.nix { };
   spacecake = pkgs.callPackage ./spacecake/package.nix { };
