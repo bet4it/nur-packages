4d8daf00e38633b1867fe6cfcb9179a19bb987fd
bet4it <16643669+bet4it@users.noreply.github.com>
2026-06-02 07:40:29 +0000

    Update aionui to 2.1.9

--- pkgs/aionui/package.nix
+++ pkgs/aionui/package.nix
@@ -37,13 +37,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "aionui";
-  version = "2.1.8";
+  version = "2.1.9";
 
   src = fetchFromGitHub {
     owner = "iOfficeAI";
     repo = "AionUi";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-ms4ulfkB+/HCB/nQFFqbNviIoD8EWYwwI0OcKkQRLMI=";
+    hash = "sha256-OvXS9+RdROwjy0o3ksC5sFLZ0IjQWEglJ8iit739KOM=";
   };
 
   node_modules = stdenv.mkDerivation {
