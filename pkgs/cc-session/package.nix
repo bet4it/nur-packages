0b377ebc47f0e9b04df42fab2271ad3ad8e41aae
Bet4 <0xbet4@gmail.com>
2026-06-06 11:03:58 +0800

    Add cc-session package (#206)

---
+++ pkgs/cc-session/package.nix
@@ -0,0 +1,115 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  nix-update-script,
+  rustPlatform,
+  cargo-tauri,
+  fetchNpmDeps,
+  npmHooks,
+  nodejs_22,
+  pkg-config,
+  wrapGAppsHook4,
+  at-spi2-atk,
+  cairo,
+  gdk-pixbuf,
+  glib,
+  glib-networking,
+  gsettings-desktop-schemas,
+  gtk3,
+  hicolor-icon-theme,
+  libayatana-appindicator,
+  librsvg,
+  libsoup_3,
+  openssl,
+  pango,
+  webkitgtk_4_1,
+  darwin,
+}:
+
+let
+  linuxLibraries = [
+    at-spi2-atk
+    cairo
+    gdk-pixbuf
+    glib
+    glib-networking
+    gsettings-desktop-schemas
+    gtk3
+    hicolor-icon-theme
+    libayatana-appindicator
+    librsvg
+    libsoup_3
+    openssl
+    pango
+    webkitgtk_4_1
+  ];
+
+  darwinFrameworks = lib.optionals stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      AppKit
+      CoreServices
+      Security
+      WebKit
+    ]
+  );
+in
+rustPlatform.buildRustPackage rec {
+  pname = "cc-session";
+  version = "0.4.8";
+
+  src = fetchFromGitHub {
+    owner = "tyql688";
+    repo = "cc-session";
+    rev = "v${version}";
+    hash = "sha256-CJalopNUSbX3sOsY2KaMwL5092hFFNRC9GrHZ14BvEc=";
+  };
+
+  cargoRoot = "src-tauri";
+  buildAndTestSubdir = "src-tauri";
+  cargoHash = "sha256-W+LSCqWNP0m3cdQsB7KDvEBpmRIohfoXYU0tVUasFCQ=";
+
+  npmDeps = fetchNpmDeps {
+    name = "${pname}-${version}-npm-deps";
+    inherit src;
+    hash = "sha256-iFThT4MMetsdjXVbgL9Xgvl2dq4uQ3a5B5vHT9YT220=";
+  };
+
+  nativeBuildInputs = [
+    cargo-tauri.hook
+    npmHooks.npmConfigHook
+    nodejs_22
+    pkg-config
+    wrapGAppsHook4
+  ];
+
+  buildInputs = [
+    openssl
+  ]
+  ++ lib.optionals stdenv.isLinux linuxLibraries
+  ++ darwinFrameworks;
+
+  doCheck = false;
+
+  postPatch = ''
+    substituteInPlace src-tauri/tauri.conf.json \
+      --replace-fail '"createUpdaterArtifacts": true' '"createUpdaterArtifacts": false'
+  '';
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--subpackage=npmDeps"
+      "--url=https://github.com/tyql688/cc-session"
+      "--use-github-releases"
+    ];
+  };
+
+  meta = {
+    description = "Desktop app for browsing local AI coding sessions";
+    homepage = "https://github.com/tyql688/cc-session";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "cc-session";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}
