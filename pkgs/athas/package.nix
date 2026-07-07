c075ade2f532199a1b557ab74ddc2f9d12848aa6
Bet4 <0xbet4@gmail.com>
2026-07-07 17:59:52 +0800

    Update athas to 0.8.1 (#275)
    * Update athas to 0.8.1
    
    * fix: add cacert to fix Vite+ SSL certificate error in Nix sandbox
    
    Vite+ (used by athas 0.8.1) panics during build because it cannot find
    CA certificates in the Nix sandbox environment. Add cacert to
    nativeBuildInputs and set SSL_CERT_FILE to provide the system CA bundle.
    
    Error: failed to initialize HTTP client: No CA certificates were loaded from the system

--- pkgs/athas/package.nix
+++ pkgs/athas/package.nix
@@ -12,6 +12,7 @@
   pkg-config,
   wrapGAppsHook4,
   zig,
+  cacert,
   glib-networking,
   libayatana-appindicator,
   openssl,
@@ -21,18 +22,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "athas";
-  version = "0.8.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "athasdev";
     repo = "athas";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Hwk7X5l+V8fuVVywfawupendU7716o7m5ODvXQDmqoU=";
+    hash = "sha256-HLRWeu/Gnrs2O/QfdfbUAZ2cUsL1V2WTgEc9Pdp3B34=";
   };
 
   buildAndTestSubdir = "src-tauri";
 
-  cargoHash = "sha256-tjW8ShONyDtuZwbSvIhI3E6IvNBWxTEvSKcpSNzURlA=";
+  cargoHash = "sha256-Crqv9UXYQw1sovnmQVQ4MRiLJwvciH7tDdUDcZinsWE=";
 
   doCheck = false;
 
@@ -72,7 +73,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     '';
 
     dontFixup = true;
-    outputHash = "sha256-aAh93DbPLDrGKa9Gy4id+havRGKRaenSUkdj4UAFgQE=";
+    outputHash = "sha256-1KoaX+o1F0Qibm/SRC6n/kquZQWGoSHhigs1bs7Jcf4=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
@@ -105,6 +106,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     wrapGAppsHook4
     zig
     desktop-file-utils
+    cacert
   ];
 
   buildInputs = [
@@ -118,6 +120,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     LIBCLANG_PATH = "${lib.getLib llvmPackages.libclang}/lib";
     OPENSSL_NO_VENDOR = true;
     TAURI_SKIP_VERSION_CHECK = "1";
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
   };
 
   postInstall = ''
